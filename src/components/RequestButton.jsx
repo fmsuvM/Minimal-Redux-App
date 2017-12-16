8f91dec2fd5ea3226363041de2aa0aea4a955e84
fmsuvM <k222.tkd.fms@gmail.com>
2017-12-16 16:29:34 +0900

    Feat: add form

--- src/components/RequestButton.jsx
+++ src/components/RequestButton.jsx
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Debug from 'debug';
+
+import { requestData } from '../actions/index';
 
 const debug = Debug('ocreilly::components::RequestButton');
 
-export default class RequestButton extends Component {
+class RequestButton extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            searchId: 0
+        };
+
+        this.onClick = (event) => {
+            this.props.getPokemonFromId(this.state.searchId);
+        };
+
+        this.handleIdChange = (event) => {
+            this.setState({searchId: event.target.value});
+        };
     }
 
     render() {
@@ -19,7 +34,11 @@ export default class RequestButton extends Component {
                     }
                 `}</style>
                 <div className="request-button">
-                    <a className="action-button">
+                    <input 
+                        value={this.state.searchId}
+                        onChange={this.handleIdChange}
+                    />
+                    <a className="action-button" onClick={this.onClick}>
                         <h3>Request Button 2017</h3>
                     </a>
                 </div>
@@ -27,3 +46,17 @@ export default class RequestButton extends Component {
         )
     }
 }
+
+RequestButton.propTypes = {
+    getPokemonFromId: PropTypes.func
+}
+
+const mapDispatchToProps = (dispatch) => (
+    {
+        getPokemonFromId: (id) => {
+            dispatch(requestData(id));
+        }
+    }
+);
+
+export default connect(null, mapDispatchToProps)(RequestButton);
