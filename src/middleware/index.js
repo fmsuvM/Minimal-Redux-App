b29e6e5b8276753b2b5862d965176eff9e51e43b
fmsuvM <k222.tkd.fms@gmail.com>
2017-12-16 16:30:51 +0900

    Feat: add Epics

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,12 +1,31 @@
 import { combineEpics, createEpicMiddleware } from 'redux-observable';
 import Rx from 'rxjs';
 import Debug from 'debug';
+import api from '../utils/apiUtils';
 
 import {
     REQUEST_DATA,
     FETCHING_PROJECTS,
-    RECEIVE_DATA
-} from '../actions/index.js';
+    RECEIVE_DATA,
+    requestData,
+    fetchingProjects,
+    receiveData
+} from '../actions/index';
 
 const debug = Debug('ocreilly::middleware::index');
 
+const requestDataEpic = (action$, store) =>
+    action$.ofType(REQUEST_DATA)
+        .do(_ => store.dispatch(fetchingProjects()))
+        .switchMap(action => {
+            const{ id } = action.payload;
+            return api.getPokemonNameFromId(id)
+        })
+        .map(({ data }) => {
+            return receiveData(data)
+        })
+;
+
+export default createEpicMiddleware(combineEpics(
+    requestDataEpic
+));
