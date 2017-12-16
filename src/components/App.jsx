9d14af990546faa75ecd21673ccf1c7e54d9a447
fmsuvM <k222.tkd.fms@gmail.com>
2017-12-16 16:28:57 +0900

    Feat: add redux system

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'rxjs';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import qs from 'qs';
+import Debug from 'debug';
 
 import RequestButton from './RequestButton';
 import Explanation from './Explanation';
 import Result from './Result';
 
-ReactDOM.render(
-    <div className="body">
-        <Explanation />
-        <RequestButton />
-        <Result />
-    </div>,
-    document.getElementById('app')
-);
+import reducers from '../reducers/index';
+import epicsMiddleware from '../middleware/index';
+import api from '../utils/apiUtils';
+
+const debug = Debug('ocreilly::components::App');
+const isDev = qs.parse(location.search.replace('?', ''))['isDev'] || false;
+
+window.addEventListener('DOMContentLoaded', () => {
+    debug('===> Mount App');
+    const composeEnhancers = isDev ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose : compose;
+    const store = createStore(reducers,
+        composeEnhancers(applyMiddleware(
+            epicsMiddleware
+        )));
+    api.init(store)
+    ReactDOM.render(
+        <Provider store={store}>
+            <div className="body">
+                <Explanation />
+                <RequestButton />
+                <Result />
+            </div>
+        </Provider>,
+        document.getElementById('app')
+    );
+})
